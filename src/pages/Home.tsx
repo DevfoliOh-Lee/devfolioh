2b4a973b39db328c38d8e1356358a0b0da58123c
YeonJeongLee <yeonjeong.lee00@gmail.com>
2021-11-17 18:44:48 +0900

    [#1]markup - home
    menu bar, post, post grid component 사용해서 home page markup 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import classNames from 'classnames/bind';
+
+/*Internal */
+import styles from './Home.module.scss';
+import MenuBar from '@components/HomePage/MenuBar';
+import PostGrid from '@components/HomePage/PostGird/PostGrid';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={cx('home')}>
+      <div className={cx('list')}>
+        <MenuBar />
+        <PostGrid />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
