70b1609b005eb6ac2d97853d326e57dbcdd18d64
YeonJeongLee <yeonjeong.lee00@gmail.com>
2021-11-21 21:05:38 +0900

    [#8] Home 오류 수정
    - InfiniteScroll 사용해서 전체 데이터 불러오기 완성
    - axios로 직접 불러오는건 되는데, postAPI 사용해서 하면 post가 여러번 생성되서 오류난다.
    - 위의 오류에 대해서 생각해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from '@styles/GlobalStyles';
-import { useEffect, useState } from 'react';
-import { PostAPI } from '@api/postAPI';
 import Home from '@pages/Home';
 
 function App() {
-  const [totalResult, setTotalResult] = useState<number>(0);
-
-  useEffect(() => {
-    PostAPI.getTotalResult().then((data) => {
-      setTotalResult(data.totalResults);
-    });
-  }, [totalResult]);
-
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Home pages={totalResult} />
+      <Home />
     </BrowserRouter>
   );
 }
