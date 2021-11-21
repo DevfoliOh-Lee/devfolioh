d98e8d8ec6bf99b3c6567c4426bf69bc5db15746
YeonJeongLee <yeonjeong.lee00@gmail.com>
2021-11-21 14:15:31 +0900

    [#1] api 연결, css 수정
    - post 가져와서 연결
    - 전체 css 수정
    - App에서 pageResult를 부름
    - Home에서 post값 부르는 동시에 정렬
    - PostGrid로 정렬데이터 보내서 데이터 보여줌
    - 정렬 시 업데이트 되는 문제 해결하기...!

--- src/components/HomePage/MenuBar/MenuBar.tsx
+++ src/components/HomePage/MenuBar/MenuBar.tsx
@@ -10,22 +10,26 @@ const cx = classNames.bind(styles);
 
 function MenuBar() {
   return (
-    <div className={cx('menu-bar')}>
-      <div className={cx('menu-bar-left')}>
-        <span className={cx('company-logo')}>velog</span>
-      </div>
-      <div className={cx('menu-bar-right')}>
-        <Link to="/" className={cx('search')}>
-          <IoSearchOutline />
-        </Link>
-        <button className={cx('new-post-btn')}>새 글 작성</button>
-        <div className={cx('user')}>
-          <img
-            className={cx('user-profile')}
-            src="https://avatars.githubusercontent.com/u/67946956?v=4"
-            alt="profile"
-          ></img>
-          <IoMdArrowDropdown className={cx('drop-down')} />
+    <div className={cx('top')}>
+      <div className={cx('menu-bar')}>
+        <div className={cx('menu-bar-left')}>
+          <Link to="/">
+            <span className={cx('company-logo')}>velog</span>
+          </Link>
+        </div>
+        <div className={cx('menu-bar-right')}>
+          <Link to="/" className={cx('search')}>
+            <IoSearchOutline />
+          </Link>
+          <button className={cx('new-post-btn')}>새 글 작성</button>
+          <div className={cx('user')}>
+            <img
+              className={cx('user-profile')}
+              src="https://avatars.githubusercontent.com/u/67946956?v=4"
+              alt="profile"
+            ></img>
+            <IoMdArrowDropdown className={cx('drop-down')} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/HomePage/PostGird/PostGrid.tsx
+++ src/components/HomePage/PostGird/PostGrid.tsx
@@ -3,21 +3,22 @@ import classNames from 'classnames/bind';
 /*Internal */
 import styles from './PostGrid.module.scss';
 import Post from '../Post';
-
+import { APostType } from 'src/typings/apost.interface';
 const cx = classNames.bind(styles);
 
-function PostGrid() {
+interface PostGridProps {
+  posts: APostType[];
+}
+
+function PostGrid({ posts }: PostGridProps) {
   return (
-    <div className={cx('scroll-container')}>
-      <div className={cx('container')}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+    <div className={cx('post-grid')}>
+      <div className={cx('scroll-container')}>
+        <div className={cx('container')}>
+          {posts.map((data) => (
+            <Post key={data.id} post={data} />
+          ))}
+        </div>
       </div>
     </div>
   );
